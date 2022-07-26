a545013426379cb1b7762f5c4c6dc0eb2b5cc5f8
sunyi <18016483139@163.com>
2022-07-26 22:08:14 +0800

    练习1

---
+++ src/app/app.vue
@@ -0,0 +1,54 @@
+<template>
+    <h3>{{ name }}</h3>
+   
+</template>
+
+<script>  
+
+    export default {
+
+        data(){   //放数据的
+            return{
+                name: 'sunyi',
+               
+            };
+        },
+
+        //computed一个对象，可操作下数据，
+        //,不同data ,可用方法返回，DEMO里可直接{{ processStatus }},
+        computed: {  
+           
+        },
+
+        //watch是监视某数据的变化，从而进行一些操作。
+        //要监听name ，有两个参数 ，新的值和旧的值。参数取名 newName , oldName
+        watch: { 
+           
+        },
+
+        created(){   //生命周期之一
+           
+        },
+
+        methods: {  //写方法的地方。
+          
+        }
+        
+
+    }
+
+</script>
+
+<style >
+input[type='text'],
+textarea,
+select{
+    padding: 4px 8px;
+    margin: 8px 0;
+    margin-right: 3px;
+    font-size: 16px;
+}
+label {
+    margin-left:4px;
+}
+</style>
