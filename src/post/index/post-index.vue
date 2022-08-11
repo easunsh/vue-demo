b286947ee582d8418374af19f43cd16e03c2bc98
sunyi <18016483139@163.com>
2022-08-11 21:18:49 +0800

    axios地址的配置文件

--- src/post/index/post-index.vue
+++ src/post/index/post-index.vue
@@ -1,1 +1,11 @@
 <template> <div>post index</div> </template>
+
+<script>
+import { apiHttpClient } from '@/app/app.service';
+export default {
+  async created() {
+    const response = await apiHttpClient.get('/posts');
+    console.log(response);
+  },
+};
+</script>
